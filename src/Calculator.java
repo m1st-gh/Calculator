f1b8e7c138b553eb56065ed09e55ae142e14c14e
kush <kushpatel169@gmail.com>
2022-06-04 20:35:40 -0400

    Calc

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculator extends JFrame {
+
+    public Calculator(){
+
+        //Title
+        JFrame calc = new JFrame("Calculator");
+        calc.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        //Text box
+        JTextField input = new JTextField();
+
+
+        JButton zero, one, two, three, four, five, six, seven, eight, nine;
+
+
+
+    }
+
+}
