02a88ca3e6fcb7addeb356352348c4c94b999840
m1st-gh <93106448+m1st-gh@users.noreply.github.com>
2022-06-10 16:41:11 -0400

    CodeCleanup

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Calculator extends JFrame {
@@ -9,7 +7,6 @@ public class Calculator extends JFrame {
     public Calculator() {
 
         ArrayList<String> exp = new ArrayList<>();
-
         //panel and frame
         JFrame frame = new JFrame("Calculator");
         JPanel panel = new JPanel();
@@ -32,241 +29,183 @@ public class Calculator extends JFrame {
         JButton solve = new JButton("Solve");
         JButton clr = new JButton("C");
 
-        clr.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.clear();
-                input.setText("");
-            }
+        clr.addActionListener(e -> {
+            exp.clear();
+            input.setText("");
         });
-        zero.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("0");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                    System.out.println(exps);
+        zero.addActionListener(e -> {
+            exp.add("0");
+            String exps = "";
+            for (String s : exp) {
+                exps += s;
+                System.out.println(exps);
+            }
+            input.setText(exps);
+        });
+        one.addActionListener(e -> {
+            exp.add("1");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+        });
+        two.addActionListener(e -> {
+            exp.add("2");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+        });
+        three.addActionListener(e -> {
+            exp.add("3");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+        });
+        four.addActionListener(e -> {
+            exp.add("4");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+        });
+        five.addActionListener(e -> {
+            exp.add("5");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+        });
+        six.addActionListener(e -> {
+            exp.add("6");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+        });
+        seven.addActionListener(e -> {
+            exp.add("7");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+        });
+        eight.addActionListener(e -> {
+            exp.add("8");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+        });
+        nine.addActionListener(e -> {
+            exp.add("9");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+        });
+        add.addActionListener(e -> {
+            exp.add("+");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+        });
+        sub.addActionListener(e -> {
+            exp.add("-");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+        });
+        div.addActionListener(e -> {
+            exp.add("/");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+        });
+        mul.addActionListener(e -> {
+            exp.add("*");
+            StringBuilder exps = new StringBuilder();
+            for (String s : exp) {
+                exps.append(s);
+            }
+            input.setText(exps.toString());
+
+
+        });
+        solve.addActionListener(e -> {
+            int i;
+
+            for (i = 0; i < exp.size(); i++) {
+                if (exp.get(i).equals("+") || exp.get(i).equals("-") || exp.get(i).equals("*") || exp.get(i).equals("/")) {
+                    break;
                 }
-                input.setText(exps);
+            }
+            StringBuilder numString1 = new StringBuilder();
+            StringBuilder numString2 = new StringBuilder();
+
+            for (int j = 0; j < i; j++) {
+                numString1.append(exp.get(j));
 
             }
-        });
-        one.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("1");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
+            for (int j = i + 1; j < exp.size(); j++) {
+                numString2.append(exp.get(j));
 
             }
-        });
-        two.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("2");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-        three.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("3");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-        four.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("4");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-        five.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("5");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-        six.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("6");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-
-                input.setText(exps);
-
-            }
-        });
-        seven.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("7");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-        eight.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("8");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-        nine.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("9");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-
-        add.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("+");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-
-        sub.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("-");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
-
-        div.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("/");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
-
-            }
-        });
 
 
-        mul.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                exp.add("*");
-                String exps = "";
-                for (int i = 0; i < exp.size(); i++) {
-                    exps += exp.get(i);
-                }
-                input.setText(exps);
+            double num1 = Double.parseDouble(numString1.toString());
+            double num2 = Double.parseDouble(numString2.toString());
+            double valueOf = 0;
+            boolean isValid = true;
 
 
-            }
-        });
-        solve.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int i = 0;
-
-                for (i = 0; i < exp.size(); i++) {
-                    if (exp.get(i).equals("+") || exp.get(i).equals("-") || exp.get(i).equals("*") || exp.get(i).equals("/")) {
-                        break;
+            switch (exp.get(i)) {
+                case "+":
+                    valueOf = num1 + num2;
+                    break;
+                case "-":
+                    valueOf = num1 - num2;
+                    break;
+                case "*":
+                    valueOf = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 > 0) {
+                        valueOf = num1 / num2;
+                    } else {
+                        isValid = false;
                     }
-                }
-                String numString1 = "";
-                String numString2 = "";
+                    break;
+            }
 
-                for (int j = 0; j < i; j++) {
-                    numString1 = numString1 + exp.get(j);
-
-                }
-                for (int j = i + 1; j < exp.size(); j++) {
-                    numString2 = numString2 + exp.get(j);
-
-                }
-
-
-                double num1 = Double.parseDouble(numString1);
-                double num2 = Double.parseDouble(numString2);
-                double valueOf = 0;
-                boolean isValid = true;
-
-
-                switch (exp.get(i)) {
-                    case "+":
-                        valueOf = num1 + num2;
-                        break;
-                    case "-":
-                        valueOf = num1 - num2;
-                    case "*":
-                        valueOf = num1 * num2;
-                    case "/":
-                        if (num2 > 0) {
-                            valueOf = num1 / num2;
-                        } else {
-                            isValid = false;
-                        }
-                        break;
-                }
-
-                if (isValid) {
-                    input.setText(String.valueOf(valueOf));
-                } else {
-                    input.setText("Error");
-                }
+            if (isValid) {
+                input.setText(String.valueOf(valueOf));
+            } else {
+                input.setText("Error");
             }
         });
 
